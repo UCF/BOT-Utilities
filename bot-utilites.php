9c1601237720ea963f0cbffc3d925a53add8a5ef
RJ Bruneel <rj.bruneel@ucf.edu>
2019-08-07 13:39:52 -0400

    Updated template placeholders

---
+++ bot-utilites.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: BOT Utilities
+Description: Provides extra functionality and utilities for the BOT website.
+Version: 1.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/BOT-Utilities
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
